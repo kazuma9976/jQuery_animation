f20e0117a2bad1ff0804c997f06b419e804226dc
iwai <issin.doku2442@gmail.com>
2022-04-04 01:47:08 +0000

    jQueryアニメーション加工完成

--- js/script.js
+++ js/script.js
@@ -6,9 +6,10 @@ $(function(){
   //console.log(title);
   $('h1').text('');
   let count = 1;
-  // テキストアニメーションの関数を定義
+  // text_animation関数を定義
   const text_animation = () => {
       const word = title.substr(0, count);
+      // 挙動確認
       console.log(word);
       $('h1').text(word);
       count++;
@@ -16,66 +17,76 @@ $(function(){
           clearInterval(timer);
       }
   };
-  //text_animation();
-  //タイマー処理
+  
+  //タイマー処理、text_animation関数の実行(0,1秒間隔でタイピングのように文字を表示)。
   const timer = setInterval(text_animation, 100);
   
   // 経過秒数表示
   let sec = 1;
+  // count_up関数の定義
   const count_up = () => {
     $('p span').text(sec);
     sec++;
-   
   };
+  
+  // count_up関数の実行
   setInterval(count_up, 1000);
   
-  // 画像アニメーション1（fadein/fadeout）
-  const images = ['images/1.jpg', 'images/2.jpg', 'images/3.jpg'];
-  
+  // 画像アニメーション（fadein/fadeout）
+  const images_1 = ['images/1.jpg', 'images/2.jpg', 'images/3.jpg'];
   let ff_count = 1;
-  
+  // opacityを使用したアニメーション。
   const fadein_fadeout = () => {
       $('#fadein_fadeout img').animate({'opacity': 0}, 1000, () => {
-          $('#fadein_fadeout img').prop('src', images[ff_count]);
+          $('#fadein_fadeout img').prop('src', images_1[ff_count]);
           $('#fadein_fadeout img').animate({'opacity': 1});
           ff_count++;
-          if(ff_count === images.length){
+          if(ff_count === images_1.length){
               ff_count = 0;
           }
       });
   };
   
+  // 5秒間隔で実行
   setInterval(fadein_fadeout, 5000);
   
   //画像アニメーション(slider)
-  $.each(images, (index, image) => {
+  const images_2 = ['images/a.jpg', 'images/b.jpg', 'images/c.jpg'];
+  $.each(images_2, (index, image) => {
         $('#slider').append($('<img>', {src: image}));
     });
-    
+    // 1番目と２番目の画像をmargin-left:-500pxの位置に設定
     $('#slider img').eq(1).css('margin-left', '-500px');
     $('#slider img').eq(2).css('margin-left', '-500px');
   
   let s_count = 0;
-  
+  // sliderの非同期処理
   const slider = () => {
         $.when(
-
-            $('#slider img').eq(s_count % 3).animate({marginLeft: '500'}, 2000),
+            // 0番目の画像を、margin-left:500pxの位置に移動。
+            $('#slider img').eq(s_count % 3).animate({marginLeft: '500px'}, 2000),
+            // 1番目の画像を、margin-left:0pxの位置に移動。
             $('#slider img').eq((s_count + 1) % 3).animate({marginLeft: '0px'}, 2000),
+            // 挙動確認
             console.log('a')
         ).done(
             () => {
+                // 0番目の画像を、margin-left:-500pxの位置に移動
                 $('#slider img').eq(s_count % 3).css('margin-left', '-500px');
+                // 挙動確認
                 console.log('b');
             }
         ).done(
             () => {
+                // 次の画像の配列番号を取得するために、s_countを+1させて処理を繰り返す。
                 s_count++;
+                // 挙動確認
                 console.log('s_count = ' + s_count);
             }
         );
     };
     
+    // 5秒間隔でslider実行
     setInterval(slider, 5000);
   
 });
